766e8c8d4b1a8b698f87ec9accb7ad671482f98a
Jamie Scott <scott.jams@gmail.com>
2020-06-26 00:34:12 -0400

    Installed masonry and edited layout

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
     var state = $('.container').children().eq(i);
     //Put state name in element
     state.html("<span>" + state.attr('id') + "</span>");
-    adjustSize();
   }
-
+  adjustSize();
+  $(".container").masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    percentPosition: true,
+  });
 
 });
 function httpGet(theUrl)
@@ -21,11 +25,24 @@ function adjustSize(){
     //Grab our COVID data and parse it
     var stateData = JSON.parse(httpGet("https://covidtracking.com/api/states"));
     var usData = JSON.parse(httpGet("https://covidtracking.com/api/us"));
-    var totalCases = usData[0]['positive']
-    //Find data for state and adjust size of element accordingly
-    for(var j = 0; j < data.length; j++){
-        if(data[i]['state'] === state.attr('id')){
-             
+    var totalCases = usData[0]['positive'];
+    //Cycle through all the state elements
+    for(var i = 0; i < $('.container *').length; i++){
+        var state = $('.container').children().eq(i);
+        //Find data for state and adjust size of element accordingly
+        for(var j = 0; j < stateData.length; j++){
+            if(stateData[j]['state'] === state.attr('id')){
+                 var stateCases = stateData[j]['positive'];
+                 var percentage = (stateCases / totalCases);
+                 var area = percentage * 9721.95;
+                 var width = Math.sqrt(area);
+                 var height = area / 5;
+                 state.css('width', width + '%');
+                 if(height > 50){
+                    state.css('height', height + 'px')
+                 }
+            }
         }
     }
+    
 }
